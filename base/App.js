9cb240616f82bf51e3110065954ada65b5cbbf2b
Luis Eduardo Patt <liuspatt@gmail.com>
2019-03-13 13:06:06 -0500

    udpate app render

---
+++ base/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppRoot from './src/app_code/AppRoot';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppRoot></AppRoot>
+    );
+  }
+}
